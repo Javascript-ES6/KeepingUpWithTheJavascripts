36a86a9682fefc0b91036e7ceaf401961fce6a6c
kibiwotkosgei <collinsboen@gmail.com>
2022-12-26 15:03:22 +0300

    Switch statements

--- index.js
+++ index.js
@@ -474,6 +474,80 @@ function Dog(years,breed){
     this.type = breed;
     setInterval(() =>{
         this.age += 1;
-console.log(this);
+// console.log(this);
     },5000)
 }
+
+//switch statement
+
+/*
+switch(expression){
+    case value1:
+        ststement to be executed when result of expression matches value1
+        break;
+        case value2:
+            ...
+            case value3:
+                ..
+                break;
+                default:
+                    break;
+
+}
+*/
+
+const myFruit = "apple";
+if (myFruit==="apple"){
+    console.log("Awesome,I love apples");
+}else if(myFruit === "orange"){
+    console.log("Oranges are cool");
+}else if(myFruit === "pear"){
+    console.log("Pears are good too");
+}else{
+    console.log("That sounds pretty");
+}
+
+switch(myFruit){
+    case "apple":
+        console.log("Great I love apples");
+     break;
+     case "banana": 
+     console.log("ok");
+     break;
+     default:
+        console.log("I did not understand");
+        break;
+}
+
+function numChecker(num){
+let value;
+switch(num){
+    case 0:
+        case 1:
+            case 2:
+                value = "Low range";
+                break;
+        case 3:
+            case 4:
+                case 5:
+                value = "Mid range";
+                break;
+        case 6:
+            case 7:
+                case 8:
+                value = "High range";
+                break;
+                default:
+                    value="Input number between 0-8";
+                    console.log(value);
+                    return;
+                }                    
+setVolume(value);
+}
+
+function setVolume(n){
+    console.log("Volume set as at " + n);
+}
+numChecker(100);
+
+
